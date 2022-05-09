bd5d7bcd4b3347fe56dd6774c54df4e61289e700
pablo-davila1 <62685470+pablo-davila1@users.noreply.github.com>
2022-05-08 22:34:53 -0500

    Dependencias
    Se agrega al pom.xml las dependencias necesarias

--- kata-crud-refactoring/back/src/main/java/co/com/sofka/crud/controllers/TodoController.java
+++ kata-crud-refactoring/back/src/main/java/co/com/sofka/crud/controllers/TodoController.java
@@ -3,7 +3,9 @@ package co.com.sofka.crud.controllers;
 import co.com.sofka.crud.models.Todo;
 import co.com.sofka.crud.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
